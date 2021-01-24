55a35961e95cbe4fba1cb942801e26bb80d5ce98
jcontrerasroberto <contreras.jrobert@gmail.com>
2021-01-24 17:53:17 -0600

    Llenado de update funcionando

--- README.txt
+++ README.txt
@@ -1,9 +1,7 @@
 FALTA (ROBERT):
-Navbar
 +Estilo
 Editar adminpanel
 Editar usuario
-Boton para borrar sesion
 
 
 AGREGADOS:
@@ -11,8 +9,6 @@ Ya se puede buscar igual un estudio, musico
 
 
 BACKEND (JULIO):
-
-users.html muestra los usuarios, aqui obtienes los datos de la base de datos y creas varios div result (plantilla) con los datos de cada usuario
 
 register.html sirve para registrar un nuevo usuario via post, en caso de exito redireccionar al usuario a la vista users.html
 
@@ -22,16 +18,4 @@ Si el registro es exitoso redireccionar al usuario a la vista show_info.html?key
 Cada vista que es update_*.html recive via get un id que corresponde al id del que quieren editar, por ejemplo update_cancion.html?key=1201
 Dependiendo lo que sea se busca en la bd y los datos se vacian en los value de cada input, cada vista viene ya con un value puesto que debes cambiar
 
-
 Cambiar las rutas de post en admin.js en los metodos ajax para commitDelete() y commitDeleteUser()
-
-
-show_info.html recive via GET dos propiedades, la key=id y category=categoria, ejemplo show_info.html?key=15&category=song 
-Dependiendo de cada categoria se mostrara algo, las plantillas ya estan creadas, solo debes ver cual poner de acuerdo a la categoria y vaciar los valores
-Las categorias son:
-artist
-album
-band
-song
-musico
-estudio
---
+++ js/fill_update.js
@@ -0,0 +1,232 @@
+function llenarAlbum(){
+    //Obtener informacion del musico
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const key = urlParams.get('key');
+    console.log(key);
+
+    var elem = {
+        "id": key,
+        "type": "album"
+    };
+
+    var mostrar = JSON.stringify(elem);
+    //console.log(mostrar);
+
+    //PETICION AJAX
+
+    $.ajax({
+        url: '../test/album.json', //Cambiar
+        type: 'get',
+        dataType: 'json',
+        contentType: 'application/json',
+        success: function (resp) {
+            console.log(resp);
+            document.getElementById("id").setAttribute("value", resp.id);
+            document.getElementById("nombrealbum").setAttribute("value", resp.nombre);
+            document.getElementById("fecha_lanzamiento").setAttribute("value", resp.fechaLanzamiento);
+            document.getElementById("pistas").setAttribute("value", resp.pistas);
+            document.getElementById("banda").setAttribute("value", resp.banda);
+            document.getElementById("estudio").setAttribute("value", resp.estudio);
+            document.getElementById("portada").setAttribute("value", resp.portada);
+            document.getElementById("contraportada").setAttribute("value", resp.contraportada);
+
+        },
+        data: mostrar
+    });
+    
+}
+
+function llenarArtist(){
+    //Obtener informacion del musico
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const key = urlParams.get('key');
+    console.log(key);
+
+    var elem = {
+        "id": key,
+        "type": "artist"
+    };
+
+    var mostrar = JSON.stringify(elem);
+    //console.log(mostrar);
+
+    //PETICION AJAX
+
+    $.ajax({
+        url: '../test/artist.json', //Cambiar
+        type: 'get',
+        dataType: 'json',
+        contentType: 'application/json',
+        success: function (resp) {
+            console.log(resp);
+            document.getElementById("id").setAttribute("value", resp.id);
+            document.getElementById("nombre").setAttribute("value", resp.nombre);
+            document.getElementById("fecha_nacimiento").setAttribute("value", resp.fechaNacimiento);
+            document.getElementById("ciudad").setAttribute("value", resp.cdNacimiento);
+            if(resp.genero=="Masculino")
+                document.getElementById('masculino').checked = true;
+            else document.getElementById('femenino').checked = true;
+            document.getElementById("web").setAttribute("value", resp.web);
+            document.getElementById("biografia").value="";
+            document.getElementById("biografia").value=resp.biografia;
+
+        },
+        data: mostrar
+    });
+    
+}
+
+function llenarBanda(){
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const key = urlParams.get('key');
+    console.log(key);
+
+    var elem = {
+        "id": key,
+        "type": "band"
+    };
+
+    var mostrar = JSON.stringify(elem);
+    console.log(mostrar);
+
+    //PETICION AJAX
+
+    $.ajax({
+        url: '../test/band.json', //Cambiar
+        type: 'get',
+        dataType: 'json',
+        contentType: 'application/json',
+        success: function (resp) {
+            document.getElementById("id").setAttribute("value", resp.id);
+            document.getElementById("nombre").setAttribute("value", resp.nombre);
+            document.getElementById("fecha").setAttribute("value", resp.anio);
+            document.getElementById("ciudad").setAttribute("value", resp.ciudad);
+            document.getElementById("influencias").value = "";
+            for(let inf of resp.influencias){
+                document.getElementById("influencias").value += inf+"\n";
+            }
+        },
+        data: mostrar
+    });
+}
+
+function llenarSong(){
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const key = urlParams.get('key');
+    console.log(key);
+
+    var elem = {
+        "id": key,
+        "type": "song"
+    };
+
+    var mostrar = JSON.stringify(elem);
+    console.log(mostrar);
+
+    //PETICION AJAX
+
+    $.ajax({
+        url: '../test/song.json', //Cambiar
+        type: 'get',
+        dataType: 'json',
+        contentType: 'application/json',
+        success: function (resp) {
+            document.getElementById("id").setAttribute("value", resp.id);
+            document.getElementById("nombrecancion").setAttribute("value", resp.nombre);
+            document.getElementById("compositor").setAttribute("value", resp.compositor);
+            document.getElementById("fecha_lanzamiento").setAttribute("value", resp.fechaLanzamiento);
+            document.getElementById("duracion").setAttribute("value", resp.duracion);
+            
+            document.getElementById("observaciones").value = "";
+            document.getElementById("observaciones").value = resp.observaciones;
+            document.getElementById("invitados").value = "";
+            for(let inv of resp.invitados){
+                document.getElementById("invitados").value += inv+"\n";
+            }
+            document.getElementById("tablatura_url").setAttribute("value", resp.tablatura_url);
+            document.getElementById("tablatura_compositor").setAttribute("value", resp.tablatura_compositor);
+            document.getElementById("fecha_tablatura").setAttribute("value", resp.tablatura_fecha);
+            document.getElementById("traductor").setAttribute("value", resp.tablatura_traductor);
+            document.getElementById("email_traductor").setAttribute("value", resp.tablatura_email);
+
+        },
+        data: mostrar
+    });
+}
+
+
+function llenarEstudio(){
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const key = urlParams.get('key');
+    console.log(key);
+
+    var elem = {
+        "id": key,
+        "type": "estudio"
+    };
+
+    var mostrar = JSON.stringify(elem);
+    console.log(mostrar);
+
+    //PETICION AJAX
+
+    $.ajax({
+        url: '../test/estudio.json', //Cambiar
+        type: 'get',
+        dataType: 'json',
+        contentType: 'application/json',
+        success: function (resp) {
+            document.getElementById("id").setAttribute("value", resp.id);
+            document.getElementById("nombre").setAttribute("value", resp.nombre);
+            
+            document.getElementById("direccion").value = "";
+            document.getElementById("direccion").value = resp.direccion;
+            
+            document.getElementById("telefono").setAttribute("value", resp.telefono);
+            document.getElementById("web").setAttribute("value", resp.web);
+
+        },
+        data: mostrar
+    });
+}
+
+
+function llenarMusico(){
+    //Obtener informacion del musico
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const key = urlParams.get('key');
+    console.log(key);
+
+    var elem = {
+        "id": key,
+        "type": "musico"
+    };
+
+    var mostrar = JSON.stringify(elem);
+    console.log(mostrar);
+
+    //PETICION AJAX
+
+    $.ajax({
+        url: '../test/musico.json', //Cambiar
+        type: 'get',
+        dataType: 'json',
+        contentType: 'application/json',
+        success: function (resp) {
+            document.getElementById("id").setAttribute("value", resp.id);
+            document.getElementById("nombre").setAttribute("value", resp.nombre);
+            document.getElementById("instrumento").setAttribute("value", resp.instrumento);
+            document.getElementById("ciudad").setAttribute("value", resp.cdNacimiento);
+        },
+        data: mostrar
+    });
+    
+}
+
+
